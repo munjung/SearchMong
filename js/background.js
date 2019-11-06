7602a7b50812c83339ca4a852f5b16d899711e8d
koosy123 <koosy123@naver.com>
2019-11-06 17:56:31 +0900

    contextMenu 수정, css 아주 약간 추가

--- js/background.js
+++ js/background.js
@@ -1,18 +1,17 @@
-//contextMenus생성
 whale.contextMenus.create({
     title: '이미지 검색하기',
     contexts: ['image'],
     onclick: searchImg
 });
 
-//클릭시
+
 function searchImg(item) {
-	whale.sidebarAction.show();
-	whale.storage.local.set({
+    whale.sidebarAction.show();
+    whale.storage.local.set({
     message: item.srcUrl
-	}, () => {
+    }, () => {
     if (!whale.runtime.lastError) {
         whale.sidebarAction.show();
     }
-	});
+    });
 }
