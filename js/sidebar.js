b3806d4eabb9ef82fce4dfe02bf49fefd32b2e4b
munjung <jekang0123@gmail.com>
2019-11-06 23:54:36 +0900

    스위치 추가, ui 조금 적용

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,19 +1,17 @@
-
 var myimage = document.getElementById("mimg");
 
 window.addEventListener('DOMContentLoaded', () => {
     // 처음 로딩 될 때: 메시지가 있는지 확인하고 삭제
-    whale.storage.local.get(`message`, storage => {
-        //console.log(storage.message); // = Hello
-        //var myimage = document.getElementById("mimg");
-        if(myimage!=null)
-             myimage.src=storage.message;
+    whale.storage.local.get('message', storage => {
+      // if(myimage!=null)
+      //      myimage.src=storage.message;
+
         //whale.storage.local.remove(`message`);
     });
 
     // 로딩 이후의 변화 대응
     whale.storage.onChanged.addListener((changes, areaName) => {
-        if (areaName === `local` && `message` in changes) {
+        if (areaName === 'local' && 'message' in changes) {
             //var myimage = document.getElementById("mimg");
             myimage.src=changes.message.newValue;
             //myimage.src = 'message';
